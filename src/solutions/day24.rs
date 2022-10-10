e4b3ca68d8385b7a654b187e516a4585b6be196c
Floris Douw <fdouw@hotmail.com>
2022-10-10 14:32:03 +0200

    Add solutions to days 24 and 25

--- src/solutions/day24.rs
+++ src/solutions/day24.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 
 pub fn solve(input: String, _verbose: bool) -> (String, String) {
-    let components: Vec<(usize, (u16, u16, u32))> = input
+    let components: Vec<(usize, (u16, u16, u16))> = input
         .trim()
         .lines()
         .map(|l| {
@@ -10,34 +10,52 @@ pub fn solve(input: String, _verbose: bool) -> (String, String) {
                 .collect_tuple()
                 .unwrap()
         })
-        .map(|(a, b)| (a, b, (a + b) as u32))
+        .map(|(a, b)| (a, b, a + b))
         .enumerate()
         .collect();
 
     let mut picked: Vec<bool> = Vec::with_capacity(components.len());
     (0..components.len()).for_each(|_| picked.push(false));
-    let max_weight = dfs(&components, &mut picked, 0);
+    let (weight_part1, weight_part2, _) = dfs(&components, &mut picked, 0);
 
-    (
-        max_weight.to_string(),
-        String::from("<not yet implemented>"),
-    )
+    (weight_part1.to_string(), weight_part2.to_string())
 }
 
-fn dfs(components: &Vec<(usize, (u16, u16, u32))>, picked: &mut Vec<bool>, port: u16) -> u32 {
-    let mut weight = 0;
+fn dfs(
+    components: &Vec<(usize, (u16, u16, u16))>,
+    picked: &mut Vec<bool>,
+    port: u16,
+) -> (u16, u16, usize) {
+    let mut weight1 = 0;
+    let mut weight2 = 0;
+    let mut len = 0;
     for (i, component) in components {
         if !picked[*i] {
             if component.0 == port {
                 picked[*i] = true;
-                weight = weight.max(component.2 + dfs(components, picked, component.1));
+
+                let (w1, w2, l) = dfs(components, picked, component.1);
+                weight1 = weight1.max(component.2 + w1);
+                if l > len || (l == len && component.2 + w2 > weight2) {
+                    weight2 = component.2 + w2;
+                    len = l;
+                }
+
                 picked[*i] = false;
             } else if component.1 == port {
                 picked[*i] = true;
-                weight = weight.max(component.2 + dfs(components, picked, component.0));
+
+                let (w1, w2, l) = dfs(components, picked, component.0);
+                weight1 = weight1.max(component.2 + w1);
+                if l > len || (l == len && component.2 + w2 > weight2) {
+                    weight2 = component.2 + w2;
+                    len = l;
+                }
+
                 picked[*i] = false;
             }
         }
     }
-    weight
+    // len + 1, to account for current component
+    (weight1, weight2, len + 1)
 }
--- src/solutions/day25.rs
+++ src/solutions/day25.rs
@@ -1,6 +1,82 @@
-pub fn solve(_input: String, _verbose: bool) -> (String, String) {
-    (
-        String::from("<not yet implemented>"),
-        String::from("<not yet implemented>"),
-    )
+use std::collections::HashMap;
+
+pub fn solve(input: String, _verbose: bool) -> (String, String) {
+    let start_state = input.lines().next().unwrap().chars().nth(15).unwrap() as u8 - b'A';
+    let step_limit: usize = input
+        .lines()
+        .skip(1)
+        .next()
+        .unwrap()
+        .split_ascii_whitespace()
+        .nth(5)
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    // Assume the states are named A,B,C,... And that their definitions appear in order.
+    // Then we can store them in an ordered vector
+    let mut states = Vec::new();
+
+    // Assume all state instructions follow the same format
+    for state_data in input.trim().split("\n\n").skip(1) {
+        let mut lines = state_data.trim().lines().skip(2);
+        // let _name = lines.next().unwrap().chars().nth(9).unwrap() as u8 - b'A';
+        // lines.next();
+        // If the current value is 0:
+        let write_value_0 = lines.next().unwrap().chars().nth(22).unwrap() == '1';
+        let move_right_0 = lines
+            .next()
+            .unwrap()
+            .split_ascii_whitespace()
+            .last()
+            .unwrap()
+            == "right.";
+        let next_state_0 = lines.next().unwrap().chars().nth(26).unwrap() as u8 - b'A';
+        lines.next();
+        // If the current value is 1:
+        let write_value_1 = lines.next().unwrap().chars().nth(22).unwrap() == '1';
+        let move_right_1 = lines
+            .next()
+            .unwrap()
+            .split_ascii_whitespace()
+            .last()
+            .unwrap()
+            == "right.";
+        let next_state_1 = lines.next().unwrap().chars().nth(26).unwrap() as u8 - b'A';
+        states.push((
+            (
+                write_value_0,
+                if move_right_0 { 1 } else { -1 },
+                next_state_0 as usize,
+            ),
+            (
+                write_value_1,
+                if move_right_1 { 1 } else { -1 },
+                next_state_1 as usize,
+            ),
+        ));
+    }
+
+    // Create an empty tape and run the program
+    let mut tape = HashMap::new();
+    let mut pointer = 0isize;
+
+    let mut state_idx = start_state as usize;
+
+    for _step in 0..step_limit {
+        let value = tape.entry(pointer).or_insert(false);
+        let instruction = if *value {
+            states[state_idx].1
+        } else {
+            states[state_idx].0
+        };
+        *value = instruction.0;
+        pointer += instruction.1;
+        state_idx = instruction.2;
+    }
+
+    // Compute the checksum
+    let checksum = tape.values().fold(0, |acc, x| acc + *x as u32);
+
+    (checksum.to_string(), String::from("<no part 2 for day 25>"))
 }
